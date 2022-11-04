d916a4182885b022bfec583a97b8fc0271d09b1e
Mariela Valentini <marielavalentini@outlook.com>
2022-11-04 16:15:42 -0300

    caso 4

---
+++ Entidades/Caso 4/Alumno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Caso_4
+{
+    public class Alumno
+    {
+        public Alumno(string pNombre, string pApellido, string pCurso, Docente pDocente)
+        {
+            Nombre = pNombre;
+            Apellido = pApellido;
+            Curso = pCurso;
+            Docente = pDocente;
+        }
+        public Alumno()
+        {
+
+        }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Curso { get; set; }
+        public Docente Docente { get; set; }
+
+    }
+}
---
+++ Entidades/Caso 4/Docente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Caso_4
+{
+    public class Docente
+    {
+        public Docente(string pNombre, string pApellido, List<Alumno> plistaAlumnos, string materia)
+        {
+            Nombre = pNombre;
+            Apellido = pApellido;
+            ListaAlumnos = plistaAlumnos;
+            Materia = materia;
+        }
+        public Docente()
+        {
+
+        }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Materia { get; set; }
+        public List<Alumno> ListaAlumnos { get; set; }
+    }
+}
